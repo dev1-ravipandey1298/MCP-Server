3acc9def7d340177548978504f6556f313e9a1c5
dev1-ravipandey1298 <dev1.ravipandey1298@gmail.com>
2025-08-08 09:17:01 +0530

    Create MainClass.java

---
+++ MainClass.java
@@ -0,0 +1,23 @@
+
+package com.demo.mcp;
+
+import com.demo.mcp.service.WeatherService;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class McpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpApplication.class, args);
+	}
+
+	@Bean
+	public ToolCallbackProvider weatherTools(WeatherService weatherService) {
+		return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
+	}
+
+}
